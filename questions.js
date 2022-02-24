b3caf687b7ad34d6aa5fae93d61a0da24f6337a9
Mr. Ketan Puri <91621292+coder2708@users.noreply.github.com>
2022-02-24 08:28:40 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Is Cyber Awareness Important?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "No",
+      "Maybe",
+      "Not Sure"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What Is Meant By Cyber Awareness?",
+    answer: "evel of awareness and understanding end users have about cybersecurity best practices and the cyber threats",
+    options: [
+      "The use of a computer as an instrument to further illegal ends",
+       "level of awareness and understanding end users have about cybersecurity best practices and the cyber threats",
+      "temporal nature of the crimes committed online",
+      "harassing a person with messages, written threats, and other persistent online behavior that endangers a person's safety"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is Cyber Security Awareness for students? ",
+    answer: "Cybersecurity awareness is defined “as a methodology to educate internet users to be sensitive to the various cyber threats and the vulnerability of computers and data to these threats” (Abd Rahim, Hamid, Kiah, Shamshirband, & Furnell, 2015).",
+    options: [
+      "Cybersecurity awareness is defined “as a methodology to educate internet users to be sensitive to the various cyber threats and the vulnerability of computers and data to these threats” (Abd Rahim, Hamid, Kiah, Shamshirband, & Furnell, 2015).",
+      "Cyber security is the application of technologies, processes and controls to protect systems, networks, programs, devices and data from cyber attacks. It aims to reduce the risk of cyber attacks and protect against the unauthorised exploitation of systems, networks and technologies.",
+      "It  is a series of alternate future/universe skins in League of Legends. Set in a world where champions must band together against corporate overlords.",
+      "When an enterprise's employees are cyber security aware, it means they understand what cyber threats are, the potential impact a cyber-attack will have on their business and the steps required to reduce risk and prevent cyber-crime infiltrating their online workspace."
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
